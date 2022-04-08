4041843f40647885d4e5d3cb3135d485b3aed6ef
You-Sheng <youshaneh@gmail.com>
2022-04-07 23:29:42 -0400

    UI: refine dark mode moon icon

--- components/layout.js
+++ components/layout.js
@@ -28,7 +28,7 @@ export function Layout({ children }) {
             </nav>
             <div className="dark-mode-wrapper">
               <Switch id="darkMode" state={darkMode} setState={setDarkMode}></Switch>
-              <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} className="moon" viewBox="0 0 512 512"><path d="M32 256c0-123.8 100.3-224 223.8-224c11.36 0 29.7 1.668 40.9 3.746c9.616 1.777 11.75 14.63 3.279 19.44C245 86.5 211.2 144.6 211.2 207.8c0 109.7 99.71 193 208.3 172.3c9.561-1.805 16.28 9.324 10.11 16.95C387.9 448.6 324.8 480 255.8 480C132.1 480 32 379.6 32 256z" /></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} className={`moon ${(darkMode)? 'on' : ''}`} viewBox="0 0 512 512"><path d="M32 256c0-123.8 100.3-224 223.8-224c11.36 0 29.7 1.668 40.9 3.746c9.616 1.777 11.75 14.63 3.279 19.44C245 86.5 211.2 144.6 211.2 207.8c0 109.7 99.71 193 208.3 172.3c9.561-1.805 16.28 9.324 10.11 16.95C387.9 448.6 324.8 480 255.8 480C132.1 480 32 379.6 32 256z" /></svg>
             </div>
 
             <a className="header-menu-toggle" onClick={() => setExpanded(expanded => !expanded)}><span>Menu</span></a>
